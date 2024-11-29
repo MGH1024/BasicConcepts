5cdf403c7256d1d40747a9db17cc5c0dab98df43
MGH1024 <meysam.ghiasvand@gmail.com>
2024-11-29 08:09:45 +0330

    polymorphism example

--- OOP/Polymorphism/Program.cs
+++ OOP/Polymorphism/Program.cs
@@ -1,12 +1,20 @@
 ﻿using Polymorphism;
 
-Console.WriteLine("Overload============================");
+Console.WriteLine("Overload method or compile time polymorphism============================");
 var overloadObj = new OverloadClass();
 Console.WriteLine(overloadObj.Add(1,2));
 Console.WriteLine(overloadObj.Add(1.25M,2.45M));
 
 
-Console.WriteLine("Override============================");
+Console.WriteLine("Overload operator  or run time polymorphism ============================");
+var c1 = new ComplexNumber(1.5, 2.5);
+var c2 = new ComplexNumber(3.5, 4.5);
+var result = c1 + c2; 
+Console.WriteLine(result);
+
+
+
+Console.WriteLine("Override or run time polymorphism type 1============================");
 var animalList = new List<Animal>
 {
     new Cat(),
@@ -17,3 +25,7 @@ foreach (var animal in animalList)
 {
     Console.WriteLine(animal.GetName());
 }
+
+
+
+
